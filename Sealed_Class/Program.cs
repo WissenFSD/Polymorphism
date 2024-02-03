c756cfcb1a5bd1751ac41fe0a9caaed18f7c9522
BRIGHT <BRIGHT@DESKTOP-ST51CUU>
2024-02-03 14:24:47 +0300

    .

---
+++ Sealed_Class/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Sealed_Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
